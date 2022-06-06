d4d77b1e1a32b95bea17dfd49e501d03e4bed8eb
MasterMasterWaster <senat.alain@gmail.com>
2022-06-06 10:34:10 -0400

    Test de homepage + creation projet maquette 2

--- projetgo/pages/Create_Project.js
+++ projetgo/pages/Create_Project.js
@@ -3,7 +3,7 @@ import {useRouter} from "next/router";
 import styles from "../styles/Home.module.css";
 
 
-export default function CreateProject(){
+export default function CreateProject() {
 
     const router = useRouter();
 
@@ -14,38 +14,34 @@ export default function CreateProject(){
             </Head>
             <div className={styles.DivSousContainer}>
                 <img src="/Image_Login/logoMoon.png" className={styles.DivImageLogo}/><br/>
+                <div>
+                    <input type="text" className={styles.Input} placeholder="Nom du projet"/>
+                    <br/><br/>
+                </div>
+                <div>
 
-            <div>
+                    <input type="text" className={styles.Input} placeholder="Code du projet"/>
+                    <br/><br/>
+                </div>
+                <div>
 
-                <input type="text" className={styles.Input} placeholder="Nom du projet"/>
-                <br/><br/>
+                    <input type="text" className={styles.Input} placeholder="Description couter du projet"/>
+                    <br/><br/>
+                </div>
+                <div>
+
+                    <textarea type="text" className={styles.Input2} placeholder="Sommaire du projet"/>
+                    <br/><br/>
+                </div>
+                <diV>
+                    <button className={styles.ButtonSignIn2} onClick={() => router.push('/')}>Soumettre</button>
+                    <button className={styles.ButtonSignIn2} onClick={() => router.push('/')}>Retour</button>
+                </diV>
             </div>
-            <div>
-
-                <input type="text" className={styles.Input} placeholder="Code du projet"/>
-                <br/><br/>
-            </div>
-            <div>
-
-                <input type="text" className={styles.Input} placeholder="Description couter du projet"/>
-                <br/><br/>
-            </div>
-            <div>
-
-                <textarea type="text" className={styles.Input2} placeholder="Sommaire du projet"/>
-                <br/><br/>
-            </div>
-    <diV>
-        <button className={styles.ButtonSignIn2} onClick={() => router.push('/')}>Soumettre</button>
-        <button className={styles.ButtonSignIn2} onClick={() => router.push('/')}>Retour</button>
-    </diV>
-            </div>
-    </div>
+        </div>
 
 
-
-
-)
+    )
 
 
 }
