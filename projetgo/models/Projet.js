4f36c951c3bc1e5e7decaffb98a28761dc1078c4
gustavo-roldan <99894809+gustavo-roldan@users.noreply.github.com>
2022-06-10 09:47:22 -0400

    changement model _status Boolean

--- projetgo/models/Projet.js
+++ projetgo/models/Projet.js
@@ -4,13 +4,23 @@ import mongoose from 'mongoose';
 /* MembreSchema will correspond to a collection in your MongoDB database. */
 const ProjetSchema = new mongoose.Schema({
 
+    
+
+    _createur: String,
+
     _budget: {
 
         type: String,
         required: [true, 'Entrez le Budget .'],
         maxlength: [8, 'Budget cannot be more than 8 characters'],
     },
-
+    
+    _debut: Date,
+    _fonds: String,
+    _membres: String,
+    _status: Boolean,
+    _fin: Date,
+    
     _titre: {
    
     type: String,
@@ -31,9 +41,14 @@ _desc: {
     required: [true, 'Entrez le desc .'],
     maxlength: [6000, 'Name cannot be more than 60 characters'],
 },
+  
+},
+{
 
-  
-  
-})
+    versionKey: false // You should be aware of the outcome after set to false
+
+}
+
+)
 
 export default mongoose.models.Projet || mongoose.model('projet', ProjetSchema)
